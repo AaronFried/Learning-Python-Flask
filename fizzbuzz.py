d890d19c998450b72c9a9d12f2a39fa76b830c4b
Aaron Fried <aaron@thinkful.com>
2017-04-30 17:51:09 -0400

    Basic fizzbuzz implementation for 1 to 100

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,12 @@
 n = 100
 print("Fizz buzz counting up to " + str(n))
+
+for count in range(1, n):
+	if count % 3 == 0 and count % 5 == 0:
+		print("Fizz buzz")
+	elif count % 3 == 0:
+		print("Fizz")
+	elif count % 5 == 0:
+		print("Buzz")
+	else:
+		print(count)
